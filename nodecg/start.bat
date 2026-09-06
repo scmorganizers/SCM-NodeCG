521ebb68afc7002a7b7bf214265d55a8ed25d427
StenniHub <91371343+StenniHub@users.noreply.github.com>
2026-09-06 12:59:01 +0200

    Add bat script for running nodecg folder standalone without npm

---
+++ nodecg/start.bat
@@ -0,0 +1,3 @@
+REM Requires node to be installed on the restreaming system or a portable node.exe (https://nodejs.org/en/download) to be placed inside this folder.
+REM Make sure to run npm install inside this folder on the development system before transferring it to the restreaming system.
+node ./node_modules/nodecg/index.js
